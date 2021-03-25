1ace4be41d2b1e8949e1daa4ec72b06251477bca
Farrukh <farrukh.uet@gmail.com>
2021-03-25 13:29:00 +0500

    First commit

---
+++ resources/js/frontend/components/ExampleComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <span>Breshna Gaming Canvas</span>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
